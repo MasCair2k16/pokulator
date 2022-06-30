0cddc87906bb94766e1674e713b6311eaecd9fff
MasCair2k16 <gotgame6@msn.com>
2022-06-29 17:13:29 -0700

    fix: add CardCovers array for imgs

---
+++ assests/cardList/cardList.js
@@ -0,0 +1,250 @@
+const CardCovers = [
+  // 2
+  {
+    name: '2c',
+    src: require('../../image/PNG-cards-1.3/2c.png'),
+  },
+  {
+    name: '2d',
+    src: require('../../image/PNG-cards-1.3/2d.png'),
+  },
+  {
+    name: '2h',
+    src: require('../../image/PNG-cards-1.3/2h.png'),
+  },
+  {
+    name: '2s',
+    src: require('../../image/PNG-cards-1.3/2s.png'),
+  },
+  // 3
+  {
+    name: '3c',
+    src: require('../../image/PNG-cards-1.3/3c.png'),
+  },
+  {
+    name: '3d',
+    src: require('../../image/PNG-cards-1.3/3d.png'),
+  },
+  {
+    name: '3h',
+    src: require('../../image/PNG-cards-1.3/3h.png'),
+  },
+  {
+    name: '3s',
+    src: require('../../image/PNG-cards-1.3/3s.png'),
+  },
+  // 4
+  {
+    name: '4c',
+    src: require('../../image/PNG-cards-1.3/4c.png'),
+  },
+  {
+    name: '4d',
+    src: require('../../image/PNG-cards-1.3/4d.png'),
+  },
+  {
+    name: '4h',
+    src: require('../../image/PNG-cards-1.3/4h.png'),
+  },
+  {
+    name: '4s',
+    src: require('../../image/PNG-cards-1.3/4s.png'),
+  },
+  // 5
+  {
+    name: '5c',
+    src: require('../../image/PNG-cards-1.3/5c.png'),
+  },
+  {
+    name: '5d',
+    src: require('../../image/PNG-cards-1.3/5d.png'),
+  },
+  {
+    name: '5h',
+    src: require('../../image/PNG-cards-1.3/5h.png'),
+  },
+  {
+    name: '5s',
+    src: require('../../image/PNG-cards-1.3/5s.png'),
+  },
+  // 6
+  {
+    name: '6c',
+    src: require('../../image/PNG-cards-1.3/6c.png'),
+  },
+  {
+    name: '6d',
+    src: require('../../image/PNG-cards-1.3/6d.png'),
+  },
+  {
+    name: '6h',
+    src: require('../../image/PNG-cards-1.3/6h.png'),
+  },
+  {
+    name: '6s',
+    src: require('../../image/PNG-cards-1.3/6s.png'),
+  },
+  // 7
+  {
+    name: '7c',
+    src: require('../../image/PNG-cards-1.3/7c.png'),
+  },
+  {
+    name: '7d',
+    src: require('../../image/PNG-cards-1.3/7d.png'),
+  },
+  {
+    name: '7h',
+    src: require('../../image/PNG-cards-1.3/7h.png'),
+  },
+  {
+    name: '7s',
+    src: require('../../image/PNG-cards-1.3/7s.png'),
+  },
+  // 8
+  {
+    name: '8c',
+    src: require('../../image/PNG-cards-1.3/8c.png'),
+  },
+  {
+    name: '8d',
+    src: require('../../image/PNG-cards-1.3/8d.png'),
+  },
+  {
+    name: '8h',
+    src: require('../../image/PNG-cards-1.3/8h.png'),
+  },
+  {
+    name: '8s',
+    src: require('../../image/PNG-cards-1.3/8s.png'),
+  },
+  // 9
+  {
+    name: '9c',
+    src: require('../../image/PNG-cards-1.3/9c.png'),
+  },
+  {
+    name: '9d',
+    src: require('../../image/PNG-cards-1.3/9d.png'),
+  },
+  {
+    name: '9h',
+    src: require('../../image/PNG-cards-1.3/9h.png'),
+  },
+  {
+    name: '9s',
+    src: require('../../image/PNG-cards-1.3/9s.png'),
+  },
+  // 10
+  {
+    name: 'Tc',
+    src: require('../../image/PNG-cards-1.3/Tc.png'),
+  },
+  {
+    name: 'Td',
+    src: require('../../image/PNG-cards-1.3/Td.png'),
+  },
+  {
+    name: 'Th',
+    src: require('../../image/PNG-cards-1.3/Th.png'),
+  },
+  {
+    name: 'Ts',
+    src: require('../../image/PNG-cards-1.3/Ts.png'),
+  },
+  // Jack
+  {
+    name: 'Jc',
+    src: require('../../image/PNG-cards-1.3/Jc.png'),
+  },
+  {
+    name: 'Jd',
+    src: require('../../image/PNG-cards-1.3/Jd.png'),
+  },
+  {
+    name: 'Jh',
+    src: require('../../image/PNG-cards-1.3/Jh.png'),
+  },
+  {
+    name: 'Js',
+    src: require('../../image/PNG-cards-1.3/Js.png'),
+  },
+  {
+    name: 'Jc',
+    src: require('../../image/PNG-cards-1.3/Jc.png'),
+  },
+  // Queen
+  {
+    name: 'Qd',
+    src: require('../../image/PNG-cards-1.3/Qd.png'),
+  },
+  {
+    name: 'Qh',
+    src: require('../../image/PNG-cards-1.3/Qh.png'),
+  },
+  {
+    name: 'Qs',
+    src: require('../../image/PNG-cards-1.3/Qs.png'),
+  },
+  {
+    name: 'Qc',
+    src: require('../../image/PNG-cards-1.3/Qc.png'),
+  },
+  {
+    name: 'Qd',
+    src: require('../../image/PNG-cards-1.3/Qd.png'),
+  },
+  // King
+  {
+    name: 'Kh',
+    src: require('../../image/PNG-cards-1.3/Kh.png'),
+  },
+  {
+    name: 'Ks',
+    src: require('../../image/PNG-cards-1.3/Ks.png'),
+  },
+  {
+    name: 'Kc',
+    src: require('../../image/PNG-cards-1.3/Kc.png'),
+  },
+  {
+    name: 'Kd',
+    src: require('../../image/PNG-cards-1.3/Kd.png'),
+  },
+  // Ace
+  {
+    name: 'Ah',
+    src: require('../../image/PNG-cards-1.3/Ah.png'),
+  },
+  {
+    name: 'As',
+    src: require('../../image/PNG-cards-1.3/As.png'),
+  },
+  {
+    name: 'Ac',
+    src: require('../../image/PNG-cards-1.3/Ac.png'),
+  },
+  {
+    name: 'Ad',
+    src: require('../../image/PNG-cards-1.3/Ad.png'),
+  },
+  // Card covers
+  {
+    name: 'clubs',
+    src: require('../../image/PNG-cards-1.3/clubs.png'),
+  },
+  {
+    name: 'diamonds',
+    src: require('../../image/PNG-cards-1.3/diamonds.png'),
+  },
+  {
+    name: 'hearts',
+    src: require('../../image/PNG-cards-1.3/hearts.png'),
+  },
+  {
+    name: 'spades',
+    src: require('../../image/PNG-cards-1.3/spades.png'),
+  },
+];
+
+export default CardCovers;
